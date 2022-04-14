709dc6d194bb24c6f2fab79ec39075d1750d13d1
Rodrigo <rodrigo.gviva@gmail.com>
2022-04-14 08:57:14 -0300

    autenticacao com jwt

--- src/components/Header/SearchBox.tsx
+++ src/components/Header/SearchBox.tsx
@@ -1,32 +1,34 @@
 import { Flex, Icon, Input } from "@chakra-ui/react";
-import {RiSearchLine} from 'react-icons/ri'
+import { useRef } from "react";
+import { RiSearchLine } from "react-icons/ri";
 
-export function SearchBox(){
-  return(
+export function SearchBox() {
+  const searchInputRef = useRef<HTMLInputElement>(null);
+
+  return (
     <Flex
-        as="label"
-        flex="1"
-        py="5"
-        px="8"
-        ml="6"
-        maxWidth={400}
-        alignSelf="center"
-        color="gray.200"
-        bg="gray.800"
-        borderRadius="full"
-        position="relative"
-      >
-        <Input 
-          color="gray.50"
-          variant="unstyled"
-          px="4"
-          mr="4"
-          placeholder="Buscar na platlaforma"
-          _placeholder={{color:'gray.400'}}
-        />
-        <Icon as={RiSearchLine} fontSize="20" />
-
-        
-      </Flex>
-  )
+      as="label"
+      flex="1"
+      py="5"
+      px="8"
+      ml="6"
+      maxWidth={400}
+      alignSelf="center"
+      color="gray.200"
+      bg="gray.800"
+      borderRadius="full"
+      position="relative"
+    >
+      <Input
+        color="gray.50"
+        variant="unstyled"
+        px="4"
+        mr="4"
+        placeholder="Buscar na platlaforma"
+        _placeholder={{ color: "gray.400" }}
+        ref={searchInputRef}
+      />
+      <Icon as={RiSearchLine} fontSize="20" />
+    </Flex>
+  );
 }
--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -13,8 +13,67 @@ import { Header } from "../../components/Header";
 import { Sidebar } from "../../components/Sidebar/";
 import { Input } from "../../components/Form/Input";
 import Link from "next/link";
+import { SubmitHandler, useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useMutation } from "react-query";
+import { api } from "../../services/api";
+import { queryClient } from "../../services/queryClient";
+import { useRouter } from "next/router";
+
+type CreateUserFormData = {
+  name: string;
+  email: string;
+  password: string;
+  password_confirmation: string;
+};
+
+const createUserFormSchema = yup.object().shape({
+  name: yup.string().required("Nome obrigatória"),
+  email: yup.string().required("E-mail obrigatório").email("E-mail inválido"),
+  password: yup
+    .string()
+    .required("Senha obrigatória")
+    .min(6, "No mínimo 6 caracteres"),
+  password_confirmation: yup
+    .string()
+    .oneOf([null, yup.ref("password")], "As senhas precisam ser iguais"),
+});
 
 export default function CreateUser() {
+  const router = useRouter();
+
+  const createUser = useMutation(
+    async (user: CreateUserFormData) => {
+      const response = await api.post("users", {
+        user: {
+          ...user,
+          created_at: new Date(),
+        },
+      });
+      return response.data.user;
+    },
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries("users");
+      },
+    }
+  );
+
+  const { register, handleSubmit, formState } = useForm({
+    resolver: yupResolver(createUserFormSchema),
+  });
+
+  const { errors } = formState;
+
+  const handleCreateUser: SubmitHandler<CreateUserFormData> = async (
+    values
+  ) => {
+    await createUser.mutateAsync(values);
+
+    router.push("/users");
+  };
+
   return (
     <Box>
       <Header />
@@ -22,7 +81,14 @@ export default function CreateUser() {
       <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
         <Sidebar />
 
-        <Box flex="1" borderRadius={8} bg="gray.800" p={["6", "8"]}>
+        <Box
+          as="form"
+          flex="1"
+          borderRadius={8}
+          bg="gray.800"
+          p={["6", "8"]}
+          onSubmit={handleSubmit(handleCreateUser)}
+        >
           <Heading size="lg" fontWeight="normal">
             Criar usuário
           </Heading>
@@ -30,16 +96,35 @@ export default function CreateUser() {
 
           <VStack spacing="8">
             <SimpleGrid minChildWidth="240px" spacing={["6", "8"]} w="100%">
-              <Input name="name" label="Nome Completo"></Input>
-              <Input name="email" type="email" label="E-mail"></Input>
+              <Input
+                name="name"
+                label="Nome Completo"
+                error={errors.name}
+                {...register("name")}
+              />
+              <Input
+                name="email"
+                type="email"
+                label="E-mail"
+                error={errors.email}
+                {...register("email")}
+              />
             </SimpleGrid>
 
             <SimpleGrid minChildWidth="240px" spacing={["6", "8"]} w="100%">
-              <Input name="passoword" type="password" label="Senha"></Input>
               <Input
-                name="passoword_confrimation"
+                name="password"
                 type="password"
-                label="Confirmaço da senha"
+                label="Senha"
+                error={errors.password}
+                {...register("password")}
+              ></Input>
+              <Input
+                name="password_confirmation"
+                type="password"
+                label="Confirmação da senha"
+                error={errors.password_confirmation}
+                {...register("password_confirmation")}
               ></Input>
             </SimpleGrid>
 
@@ -50,7 +135,13 @@ export default function CreateUser() {
                     Cancelar
                   </Button>
                 </Link>
-                <Button colorScheme="pink">Salvar</Button>
+                <Button
+                  type="submit"
+                  colorScheme="pink"
+                  isLoading={formState.isSubmitting}
+                >
+                  Salvar
+                </Button>
               </HStack>
             </Flex>
           </VStack>
